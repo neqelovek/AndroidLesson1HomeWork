e65a4617a9d6fa3cae7d0ae01f16f900a13f7689
neqelovek <neqelovek@gmail.com>
2021-12-12 15:14:45 +0300

    EditText finish

--- app/src/main/java/ru/gb/androidlesson1/MainActivity.java
+++ app/src/main/java/ru/gb/androidlesson1/MainActivity.java
@@ -3,12 +3,40 @@ package ru.gb.androidlesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer argument1;
+    private Integer argument2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView1 = findViewById(R.id.textView_for_equals);
+        final EditText editText1 = findViewById(R.id.editText_equal1);
+        final EditText editText2 = findViewById(R.id.editText_equal2);
+        Button button = findViewById(R.id.button_for_equals);
+
+        button.setOnClickListener(v -> {
+            try {
+            argument1 = Integer.valueOf(editText1.getText().toString());
+            argument2 = Integer.valueOf(editText2.getText().toString());
+
+                if (argument1.equals(argument2)){
+                    textView1.setText("Равно!");
+                }else {
+                    textView1.setText("Не равно!");
+                }
+            } catch (Exception e) {
+                textView1.setText("Введие число!");
+            }
+        });
+
+
     }
 }
