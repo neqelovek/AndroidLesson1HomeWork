bc54817710f2fb7558a82279fb02b43ab7537219
neqelovek <neqelovek@gmail.com>
2021-12-12 15:32:32 +0300

    Switch finish

--- app/src/main/java/ru/gb/androidlesson1/MainActivity.java
+++ app/src/main/java/ru/gb/androidlesson1/MainActivity.java
@@ -2,9 +2,11 @@ package ru.gb.androidlesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,14 @@ public class MainActivity extends AppCompatActivity {
     private Integer argument1;
     private Integer argument2;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final TextView textView1 = findViewById(R.id.textView_for_equals);
+        final TextView textView2 = findViewById(R.id.textView_for_switch);
         final EditText editText1 = findViewById(R.id.editText_equal1);
         final EditText editText2 = findViewById(R.id.editText_equal2);
         Button button = findViewById(R.id.button_for_equals);
@@ -34,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 }
             } catch (Exception e) {
                 textView1.setText("Введие число!");
+            }
+        });
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch switch1 = findViewById(R.id.name_switch);
+
+        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                textView2.setText("Alarm on!");
+            } else {
+                textView2.setText("");
             }
         });
 
